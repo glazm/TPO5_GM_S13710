a4a47ff380829518f92574f0a9b3401eb8984fac
Michal Glaz <michal.glaz@op.pl>
2024-06-07 14:07:53 +0200

    Trying my first servlet

---
+++ WEB-INF/classes/Comp.java
@@ -0,0 +1,5 @@
+public class Comp {
+public static void main(String[]args){
+    new Time1();
+}
+}
---
+++ WEB-INF/classes/Time1.java
@@ -0,0 +1,46 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+public class Time1 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, java.io.IOException
+    {
+        // Jaki jest preferowany język klienta?
+        Locale locale = request.getLocale();
+
+
+        // Ustalenie typu i kodowania odpowiedzi
+        // Musi być ustalone przed uzyskaniem strumienia wyjściowego
+        response.setContentType("text/html; charset=UTF-8");
+
+        // Pobranie strumienia wyjściowego
+        // z zapewnieniem własciwego kodowania
+        // czasami wystarcza samo: PrintWriter out = response.getWriter()
+
+        PrintWriter out = new PrintWriter(
+                new OutputStreamWriter(response.getOutputStream(), "UTF-8"),
+                true);
+
+
+
+        out.println("<h2>" + "Pierwszy" + "<br>" + "Servlet" + "<br>" );
+        out.println(getDate(locale) + "</h2>");
+        out.close();
+    }
+
+
+    private String getDate(Locale loc) {
+        Date data = new Date();
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.LONG,
+                DateFormat.MEDIUM,
+                loc);
+        return df.format(data);
+    }
+
+
+}
